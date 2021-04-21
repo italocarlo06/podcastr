8d1cf35235799b9255e8240dafcb761ad2bf0d80
Italo Carlo <italocarlo@nti.ufal.br>
2021-04-21 17:51:20 -0300

    Primeiro Commit Aula 2

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+
+export default function Home() {
+  return (    
+    <h1>oi</h1>  
+  )
+}
+
+export async function getStaticProps(){
+  const response= await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60*60*8 //8hours
+  }
+}
